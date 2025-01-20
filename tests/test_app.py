b32c3d4daa8ad2c498eb2b8d7af156c1e763aa94
Danilo Salles <danilosalles4@outlook.com>
2025-01-20 02:25:02 -0300

    Alterando banco fake para banco de dados sqlite

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_api.schemas import UserPublic
+
 
 def test_read_root_retornar_ok(client):
     response = client.get('/')
@@ -24,27 +26,52 @@ def test_create_user(client):
     }
 
 
+def test_create_user_400_usarname(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': user.username,
+            'email': 'john@example.com',
+            'password': 'testpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_400_email(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Diferent Name',
+            'email': user.email,
+            'password': 'testpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists'}
+
+
 def test_list_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'John',
-                'email': 'john@example.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_get_user(client):
-    response = client.get('/users/1')
+def test_list_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_get_user(client, user):
+    response = client.get(f'/users/{user.id}')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'id': 1,
-        'username': 'John',
-        'email': 'john@example.com',
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
     }
 
 
@@ -54,7 +81,7 @@ def test_get_user_404(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -86,7 +113,7 @@ def test_update_user_404(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
